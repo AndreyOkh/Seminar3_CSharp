618a410a22722bd71939ace00c7c263620dd4701
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-13 04:47:30 +1000

    Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.

---
+++ Projects_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+int n = ReadInt("Введите число: ");
+int count = 1;
+
+while (count < n + 1)
+{
+    Console.Write($"{count * count} ");
+    count++;
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
