fc891d631a7f857ec45fec7b660ba0eea9ec338d
Андрей Охотников <andrey1q2q@gmail.com>
2022-08-13 04:10:19 +1000

    Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).

---
+++ Projects_002/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int x = ReadInt("Введите четверть: ");
+
+if (x == 1)
+{
+    Console.WriteLine("x > 0 && y > 0.");
+}
+else if (x == 2)
+{
+    Console.WriteLine("x < 0 && y > 0");
+}
+else if (x == 3)
+{
+    Console.WriteLine("x < 0 && y < 0");
+}
+else if (x == 4)
+{
+    Console.WriteLine("x > 0 && y < 0");
+}
+else 
+{
+    Console.WriteLine("Нет такой четверти!");
+}
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
